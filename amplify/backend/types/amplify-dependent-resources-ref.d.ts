7a86cf263269c0796dc4aeb4a169d726cb5d775b
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-18 10:43:58 -0400

    initalized amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tasktracker": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
