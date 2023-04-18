2c838d0ce65b57a9185c2683aed5435823cade92
Alicia Liu <alicial@cs.uchicago.edu>
2023-04-19 00:01:06 +0200

    added basic UI

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,28 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, Divider, Box } from "@mui/material";
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
 
 const Sidebar = () => {
   return (
     <Stack spacing={4}>
-      <Typography>DATE component / Wed, March 29</Typography>
-      <Typography>Sort By / Radio buttons</Typography>
-      <Typography>Task Completed component</Typography>
+      <Divider >TODAY'S DATE</Divider>
+        <Typography className = "heading" sx={{ margin: "20px" }} variant="h2">Wed, March 29 2023</Typography>
+      <Divider>View</Divider>
+        {/* <Typography>Sort By / Radio buttons</Typography> */}
+        <FormControl>
+          <FormLabel>Sort By</FormLabel>
+          <RadioGroup
+            aria-labelledby="radio-buttons-group-label"
+            defaultValue="due"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="due" control={<Radio />} label="Soonest Due" />
+            <FormControlLabel value="title" control={<Radio />} label="Title" />
+            <FormControlLabel value="status" control={<Radio />} label="Status" />
+          </RadioGroup>
+        </FormControl>
+      <Divider>PROGRESS</Divider>
+        <Typography className = "heading" variant = "h2">2</Typography>
+        <Typography className = "heading" variant = "h6">Task Completed!</Typography>
     </Stack>
   )
 };
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,16 +1,22 @@
-import { Box, Card, CardHeader, CardContent, Stack, Typography } from "@mui/material";
+import { Box, Card, CardHeader, CardContent, Stack, Typography, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { TextField, Button} from "@mui/material";
+import { positions } from '@mui/system';
+import "./ToDoList.css"
 
 const ToDoList = () => {
   return (
     <Card variant='outlined'>
-      <CardHeader title='Some Header' />
+      <CardHeader title ='My Tasks' />
+          <TextField className="input" id="standard-basic" label="add a new task..." variant="standard" />
+          <Button className="input" variant = "outlined"> Add </Button>
       <CardContent>
-        <Stack spacing={2}>
-          <Typography>
-            This is the ToDo list on the right column!
-          </Typography>
+        <Stack spacing={1}>
           <Box>
-            A list of radio buttons here
+              <FormGroup>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Task 1" />
+                <FormControlLabel control={<Checkbox />} label="Task 2" />
+                <FormControlLabel control={<Checkbox />} label="Task 3" />
+              </FormGroup>
           </Box>
         </Stack>
       </CardContent>
