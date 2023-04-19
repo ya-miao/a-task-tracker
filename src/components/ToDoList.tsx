655e926a590f79d2a7f6804c504b04c0b316b4d3
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-18 20:11:57 -0400

    popup window added

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,22 +1,94 @@
 import { Box, Card, CardHeader, CardContent, Stack, Typography, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
-import { TextField, Button} from "@mui/material";
+import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
 import { positions } from '@mui/system';
-import "./ToDoList.css"
+import "./ToDoList.css";
+import { TransitionProps } from '@mui/material/transitions';
+import { useState } from "react";
+import React from "react";
+import { createTodo } from '../graphql/mutations'
+import { API } from "aws-amplify";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const handleSubmit = async () => {
+  await API.graphql({
+    query: createTodo,
+    variables: {
+      input: {
+        title: "",
+        description: "",
+        status: "",
+        dueDate: "",
+      }
+    }
+  })
+}
 
 const ToDoList = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    status: "",
+    dueDate: "",
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card variant='outlined'>
-      <CardHeader title ='My Tasks' />
-          <TextField className="input" id="standard-basic" label="add a new task..." variant="standard" />
-          <Button className="input" variant = "outlined"> Add </Button>
+      <CardHeader title='My Tasks' />
+
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add New Task
+      </Button>
+
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        fullWidth={true}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Input New Task Info"}</DialogTitle>
+        <DialogContent>
+          <Stack>
+            <TextField className="input" id="standard-basic" label="Title" variant="standard" />
+            <TextField className="input" id="standard-basic" label="Description" variant="standard" />
+            <TextField className="input" id="standard-basic" label="Status" variant="standard" />
+            <TextField className="input" id="standard-basic" label="Due Date" variant="standard" />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button onClick={handleClose}>Add</Button>
+        </DialogActions>
+      </Dialog>
+
       <CardContent>
         <Stack spacing={1}>
           <Box>
-              <FormGroup>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Task 1" />
-                <FormControlLabel control={<Checkbox />} label="Task 2" />
-                <FormControlLabel control={<Checkbox />} label="Task 3" />
-              </FormGroup>
+            <FormGroup>
+              <FormControlLabel control={<Checkbox defaultChecked />} label="Task 1" />
+              <FormControlLabel control={<Checkbox />} label="Task 2" />
+              <FormControlLabel control={<Checkbox />} label="Task 3" />
+            </FormGroup>
           </Box>
         </Stack>
       </CardContent>
