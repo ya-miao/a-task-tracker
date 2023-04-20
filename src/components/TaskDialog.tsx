bf7d00446f698b8b0c3f20038ba1c759f909a20b
tril-studio <127056177+tril-studio@users.noreply.github.com>
2023-04-19 21:21:14 -0400

    Code cleanup

---
+++ src/components/TaskDialog.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextField } from "@mui/material";
+
+interface TaskDialogProps {
+  openDialog: any,
+  setOpenDialog: any,
+  onAddTask: any,
+  onEditTask: any,
+  selectedTask: any,
+  setSelectedTask: any,
+};
+
+const TaskDialog = ({ openDialog, setOpenDialog, onAddTask, onEditTask, selectedTask, setSelectedTask }: TaskDialogProps) => {
+
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    status: "",
+    dueDate: "",
+  });
+
+  const handleClose = () => {
+    setOpenDialog(false);
+    setSelectedTask(null);
+    setForm({
+      title: "",
+      description: "",
+      status: "",
+      dueDate: "",
+    });
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value
+    })
+  };
+
+  useEffect(() => {
+    selectedTask && setForm({
+      title: selectedTask.title,
+      description: selectedTask.description,
+      status: selectedTask.status,
+      dueDate: selectedTask.dueDate,
+    });;
+  }, [selectedTask]);
+
+  return (
+    <Dialog
+      open={openDialog}
+      fullWidth={true}
+      onClose={handleClose}
+    >
+      <DialogTitle>{selectedTask ? 'Edit Task' : 'Add Task'}</DialogTitle>
+      <DialogContent>
+        <Stack>
+          <TextField defaultValue={selectedTask?.title} label="Title" variant="standard" name="title" onChange={handleInputChange} />
+          <TextField defaultValue={selectedTask?.description} label="Description" variant="standard" name="description" onChange={handleInputChange} />
+          <TextField defaultValue={selectedTask?.status} label="Status" variant="standard" name="status" onChange={handleInputChange} />
+          <TextField defaultValue={selectedTask?.dueDate} label="Due Date" variant="standard" name="dueDate" onChange={handleInputChange} />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        {selectedTask ?
+          <Button onClick={() => {
+            onEditTask(selectedTask?.id, form);
+            setOpenDialog(false);
+          }}>Edit</Button>
+          :
+          <Button onClick={() => {
+            onAddTask(form);
+            setOpenDialog(false);
+          }}>Add</Button>
+        }
+      </DialogActions>
+    </Dialog>
+  )
+};
+
+export default TaskDialog;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,132 +1,121 @@
-import { Box, Card, CardHeader, CardContent, Stack, Typography, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
-import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
-import "./ToDoList.css";
-import { TransitionProps } from '@mui/material/transitions';
+import React, { useEffect, useState } from 'react';
+
+import { Box, Card, CardHeader, CardContent, Stack, Typography } from "@mui/material";
+import { Button } from "@mui/material";
+// import "./ToDoList.css";
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@mui/icons-material/Add';
 
-import React, { useEffect, useState } from 'react';
+import TaskDialog from "./TaskDialog";
+
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import { listTodos } from '../graphql/queries';
-
 import * as mutations from '../graphql/mutations';
-import { GraphQLQuery } from '@aws-amplify/api';
+import * as subscriptions from '../graphql/subscriptions';
+import { GraphQLQuery, GraphQLSubscription } from '@aws-amplify/api';
 import { CreateTodoInput, CreateTodoMutation, DeleteTodoInput, DeleteTodoMutation, UpdateTodoInput, UpdateTodoMutation } from '../API';
+import { OnCreateTodoSubscription, OnUpdateTodoSubscription, OnDeleteTodoSubscription } from '../API';
 
 import awsmobile from "../aws-exports";
 Amplify.configure(awsmobile);
 
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & {
-    children: React.ReactElement<any, any>;
-  },
-  ref: React.Ref<unknown>,
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const ToDoList = () => {
 
-  const [ todos, setTodos ] = useState<any>([]);
+  const [open, setOpen] = useState(false);
+
+  const [todos, setTodos] = useState<any>([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
   const fetchTodos = async () => {
     try {
       const todoData: any = await API.graphql(graphqlOperation(listTodos))
       const todos: any = todoData.data.listTodos.items
-      console.log('todos: ');
-      console.log(todos);
       setTodos(todos)
     } catch (err) { console.log('error fetching todos') }
   }
 
-  const addTodo = async (form: any) => {
-      try {
-      console.log('Trying...')
+  const addTodo = async (taskInput: any) => {
+    try {
       const newTodo = await API.graphql<GraphQLQuery<CreateTodoMutation>>(
-        graphqlOperation(mutations.createTodo, { input: form })
+        graphqlOperation(mutations.createTodo, { input: taskInput })
       );
-      console.log('Succeeded!');
     } catch (err) {
       console.log('error creating todo:', err)
     }
   }
 
-  const deleteTodo = async (id: any) => {
+  const deleteTodo = async (taskId: any) => {
     try {
-      console.log('Trying...')
-      await API.graphql<GraphQLQuery<DeleteTodoMutation>>({ 
-        query: mutations.deleteTodo, 
-        variables: { input: {id: id } }
+      await API.graphql<GraphQLQuery<DeleteTodoMutation>>({
+        query: mutations.deleteTodo,
+        variables: { input: { id: taskId } }
       });
-      //
-      console.log('Succeeded!');
     } catch (err) {
       console.log('error deleting todo:', err)
     }
   }
 
-  const editTodo = async () => {
+  const editTodo = async (taskId: any, taskInput: any) => {
     try {
       console.log('Trying...')
-      const updatedTodo = await API.graphql<GraphQLQuery<UpdateTodoMutation>>({ 
-        query: mutations.updateTodo, 
-        variables: { input: {
-          id: '7788e2ce-7fb3-4314-b8c9-cf91d2e8bcf7',
-          title: 'Feed the dog',
-          description: '0.5 cup of dry food',
-        } }
+      const updatedTodo = await API.graphql<GraphQLQuery<UpdateTodoMutation>>({
+        query: mutations.updateTodo,
+        variables: {
+          input: {
+            id: taskId,
+            ...taskInput
+          }
+        }
       });
       console.log('Succeeded!');
     } catch (err) {
       console.log('error updating todo:', err)
     }
   }
-  
+
+  const subCreate = API.graphql<GraphQLSubscription<OnCreateTodoSubscription>>(
+    graphqlOperation(subscriptions.onCreateTodo)
+  ).subscribe({
+    next: ({ provider, value }) => {
+      console.log({ provider, value });
+      fetchTodos();
+    },
+    error: (error) => console.warn(error)
+  });
+
+  const subUpdate = API.graphql<GraphQLSubscription<OnUpdateTodoSubscription>>(
+    graphqlOperation(subscriptions.onUpdateTodo)
+  ).subscribe({
+    next: ({ provider, value }) => {
+      console.log({ provider, value });
+      fetchTodos();
+    },
+    error: (error) => console.warn(error)
+  });
+
+  const subDelete = API.graphql<GraphQLSubscription<OnDeleteTodoSubscription>>(
+    graphqlOperation(subscriptions.onDeleteTodo)
+  ).subscribe({
+    next: ({ provider, value }) => {
+      console.log({ provider, value });
+      fetchTodos();
+    },
+    error: (error) => console.warn(error)
+  });
+
   useEffect(() => {
     fetchTodos();
   }, []);
 
-  const [open, setOpen] = useState(false);
-
-  const [form, setForm] = useState({
-    title: "",
-    description: "",
-    status: "",
-    dueDate: "",
-  });
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setForm({
-      title: "",
-      description: "",
-      status: "",
-      dueDate: "",
-    });
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = event.target;
-    setForm({
-      ...form,
-      [name]: value
-    })
-  };
-
-  // TESTING
-  useEffect(() => {
-    console.log('form: ');
-    console.log(form);
-  }, [form])
-
   return (
     <Card variant='outlined'>
+      <TaskDialog openDialog={open} setOpenDialog={setOpen} onAddTask={addTodo} onEditTask={editTodo} selectedTask={selectedTask} setSelectedTask={setSelectedTask} />
       <Box sx={{ display: 'flex', justifyContent: "space-between" }}>
         <CardHeader title='My Tasks' />
         <Button
@@ -137,36 +126,10 @@ const ToDoList = () => {
           <AddIcon />
         </Button>
       </Box>
-
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        fullWidth={true}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Input New Task Info"}</DialogTitle>
-        <DialogContent>
-          <Stack>
-            <TextField className="input" id="standard-basic" label="Title" variant="standard" name="title" onChange={handleInputChange}/>
-            <TextField className="input" id="standard-basic" label="Description" variant="standard" name="description" onChange={handleInputChange}/>
-            <TextField className="input" id="standard-basic" label="Status" variant="standard" name="status" onChange={handleInputChange}/>
-            <TextField className="input" id="standard-basic" label="Due Date" variant="standard" name="dueDate" onChange={handleInputChange}/>
-          </Stack>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={() => {
-            addTodo(form);
-            setOpen(false);
-          }}>Add</Button>
-        </DialogActions>
-      </Dialog>
       <CardContent>
         <Stack spacing={2}>
           <Box sx={{ margin: "4px" }}>
-             {todos?.map((todo: any, index: any) => (
+            {todos?.map((todo: any, index: any) => (
               <Box sx={{ margin: "10px" }} key={index}>
                 <Stack direction="row" justifyContent="space-between" spacing={10} alignItems="flex-start">
                   <Stack direction="column">
@@ -179,7 +142,13 @@ const ToDoList = () => {
                     <Typography>{todo?.dueDate}</Typography>
                   </Stack>
                   <Stack direction="row" spacing={1}>
-                    <IconButton color="primary">
+                    <IconButton color="primary" onClick={
+                      () => {
+                        console.log('EDIT!');
+                        setSelectedTask(todo);
+                        setOpen(true);
+                      }
+                    }>
                       <EditIcon />
                     </IconButton>
                     <IconButton color="secondary" onClick={
@@ -192,7 +161,7 @@ const ToDoList = () => {
                   </Stack>
                 </Stack>
               </Box>
-             ))}
+            ))}
           </Box>
         </Stack>
       </CardContent>
